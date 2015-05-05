728729468f489685ea560cae7a3b5e2eb8806808
mliq <liquori@gmail.com>
2015-05-05 11:45:20 -0500

    Mongoose Working

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+//load express
+var express = require('express');
+// create 'instance' of Express called 'app'
+var router = express.Router();
+// Create instance of mongoose
+var mongoose = require('mongoose');
+// Connect to our mongodb database, name made up now.
+mongoose.connect('mongodb://localhost/basic_walking_skeleton');
+// declaring a basic model for our 'Cat' data that we will be bringing into the database, Cats will just have a name for now.
+var Cat = mongoose.model('Cat', {name:String});
+
+// Set up a handler for 'get' request to the home 'route'
+// Then call an anonymous function that takes in two arguments, the request object (incoming), and the response object (outgoing). In the function body, we attach “Hello!” to the response and send it back.
+// Set up a handler for 'get' request to the 'router' ?
+// Routers will help us manage how incoming requests are handled.
+// Then call an anonymous function that takes in three arguments, the request object (incoming), and the response object (outgoing), and then next has to do with how Express handles 'middleware'.
+// After response sends back 'Hello' we send an additional next() command, also Express
+router.get( '/', function(req, res, next) {
+    console.log("Here is a console log");
+    res.send('Hello World!');
+    next();
+});
+
+// issue an export order to the router to be a module
+// This makes it available to us throughout the rest of the application.
+// Basically, we have set this up to be how we handle routes for the entire application.
+module.exports = router;
