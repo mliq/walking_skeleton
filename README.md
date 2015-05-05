bec134815a6f4b058ba5a2c1ade8e7fdec43b5da
mliq <liquori@gmail.com>
2015-05-05 11:07:01 -0500

    Node and Express working

---
+++ README.md
@@ -0,0 +1,1 @@
+# Week 4 Prime - First Node Project - Walking Skeleton
--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ var express = require('express');
 // create 'instance' of Express called 'app'
 var app = express();
 
+// Load the index.js
+var index = require('../routes/index');
+// when we get a request at the root path '/' we use the index variable we created.
+app.use('/', index);
+
 // Set up a handler for 'get' request to the home 'route'
 // Then call an anonymous function that takes in two arguments, the request object (incoming), and the response object (outgoing). In the function body, we attach “Hello!” to the response and send it back.
-
-app.get( '/', function(req, res) {
-    res.send('Hello!');
-});
+//app.get( '/', function(req, res) {
+//    res.send('Hello!');
+//});
 
 // Set up a server, set equal to the listen method on the app, taking in two arguments: port we are listening on, anonymous callback function that lets us know that server is in fact up.
 // Notice that we are setting the ‘port’ variable equal to the global server variables address method, that has a port property.
