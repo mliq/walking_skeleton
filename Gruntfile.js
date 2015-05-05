5d29443a43680059c92a52c77894fe97ab36ebee
mliq <liquori@gmail.com>
2015-05-05 12:20:58 -0500

    Grunt working

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    // Project configuration.
+
+    // Grunt Initialization: pass in an object.
+    grunt.initConfig({
+        // Define file with alias pkg
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                // Reference pkg defined above and template comments that will appear at top of generated file for future reference (meta-data)
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                // Start and end files
+                src: 'client/app.js',
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                // Current working directory
+                cwd: "node_modules/",
+                // Copies this info and writes it to the 'dest' without uglifying (since it's already minified!)
+                src: [
+                    "angular/angular.min.js",
+                    "angular/angular.min.js.map",
+                    "angular/angular-csp.css"
+                ],
+                "dest": "server/public/vendor/"
+            }
+        }
+    });
+    // Register the tasks we need when we run grunt.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'uglify']);
+
+};
